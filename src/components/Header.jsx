78f92bcb19382781af599c0e523d81d21166a0cf
Dipansh Parmar <dipanshparmar870@gmail.com>
2022-05-31 09:29:24 +0530

    added login check

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,28 @@
-import { Link } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { Link, useNavigate } from 'react-router-dom';
 
 export default function Header() {
+  // selector
+  const isLogged = useSelector(state => state)
+
+  // navigate
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    if (!isLogged) {
+      navigate('/login')
+    }
+  }, [])
+
   return (
-    <div className='header'>
+    <div className='header' style={{justifyContent: !isLogged && 'center'}}>
       <h3 className='header--logo'>
         Career Point University Hamirpur
       </h3>
       <nav className='header--links'>
-        <Link to='/events' className='header--link'>Events</Link>
-        <Link to='/profile' className='header--link'>Profile</Link>
+        {isLogged && <Link to='/events' className='header--link'>Events</Link>}
+        {isLogged && <Link to='/profile' className='header--link'>Profile</Link>}
       </nav>
     </div>
   )
